c2a084559adc9dd24490866b9082be635020058e
Divesh Uttamchandani <diveshuttamchandani@gmail.com>
2018-02-17 13:44:25 +0530

    Implemente Question 2
    insertion sort and merge sort implemented

--- lab1/SeqList.h
+++ lab1/SeqList.h
@@ -26,6 +26,7 @@ typedef struct SeqList{
 
 
 SeqList newList();
+bool compare(Element e1, Element e2);
 SeqList clearList(SeqList sl);
 SeqList insertInOrder(SeqList sl, Element e);
 SeqList insertAtFront(SeqList sl, Element e);
--- lab1/SeqListUses.c
+++ lab1/SeqListUses.c
@@ -1,11 +1,31 @@
 #include "SeqListUses.h"
+#include <stdlib.h>
+#include "SeqList.h"
 
 SeqList merge(SeqList sl1, SeqList sl2){
   SeqList sl=newList();
-
-
+  while(sl1.front!=NULL || sl2.front!=NULL){
+    if(sl1.front !=NULL && (sl2.front==NULL || compare(*(sl1.front),*(sl2.front))==LESSTHAN))
+    {
+      sl=insertAtEnd(sl, *(sl1.front));
+      sl1=deleteAtFront(sl1);
+    }
+    else
+    {
+      sl=insertAtEnd(sl, *(sl2.front));
+      sl2=deleteAtFront(sl2);
+    }
+  }
   return sl;
 }
-SeqList insertionSort(SeqList sl1);
 
-
+SeqList insertionSort(SeqList sl1){
+  SeqList sl=newList();
+  Element E;
+  while(sl1.front!=NULL){
+    E=*(sl1.front);
+    sl1=deleteAtFront(sl1);
+    sl=insertInOrder(sl,E);
+  }
+  return sl;
+}
