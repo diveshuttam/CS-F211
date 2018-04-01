7fcd51156710b12933d1db4efc16c3be4008486b
Divesh Uttamchandani <diveshuttamchandani@gmail.com>
2018-04-02 01:14:59 +0530

    initial stack using seqlist

---
+++ utils/stack/Stack.h
@@ -0,0 +1,21 @@
+#ifndef __STACK_H
+#define __STACK_H
+
+#ifndef __STACK
+#define __STACK
+typedef struct Stack *Stack;
+#endif
+
+#ifndef __KEY
+#define __KEY
+typedef int Key;
+#endif
+
+typedef struct Element *Element;
+
+Stack newStack();
+Stack pop(Stack s);
+Stack push(Stack s, Element e);
+Element top(Stack s);
+Stack clearStack(Stack s);
+#endif
